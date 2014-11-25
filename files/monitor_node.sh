7f2c827c70636501cb1676861edf96e549f3c5ed
core@core1 <core@core1>
2014-11-25 17:01:55 +0900

    exportする環境変数名の衝突回避

--- files/monitor_node.sh
+++ files/monitor_node.sh
@@ -2,7 +2,7 @@
 BIN=/usr/bin/forever
 HTML=/var/www/`hostname`/html
 APP=index.js
-IS_EXEC=`cat $TEMP | grep -v 'null' | grep '/usr/bin/node /usr/lib/node_modules/forever/bin/monitor'`
+IS_EXEC=`cat $MONITOR_PS_LIST | grep -v 'null' | grep '/usr/bin/node /usr/lib/node_modules/forever/bin/monitor'`
 if [ -f $BIN -a "$IS_EXEC" = "" ];then
 	cd $HTML
     $BIN start $APP
