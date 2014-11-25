e5702e14c876541a5f7be593490debbd6970b6d7
core@core1 <core@core1>
2014-11-25 11:19:36 +0900

    journalファイルサイズ3.5Gbの空きスペース無いとmongodb起動しない対応

--- files/services.sh
+++ files/services.sh
@@ -47,8 +47,11 @@ if [ $ISDEFAULT -eq 0 ]; then
 fi
 chown -R nginx: $LOG
 
+MONGODB_CONF=/etc/mongod.conf
+sed -ri "s/#nojournal=true/nojournal=true/g" $MONGODB_CONF
+
 mkdir -p /data/db
-/usr/bin/mongod &
+/usr/bin/mongod --smallfiles &
 crontab /root/crontab.txt
 /etc/init.d/nginx start
 /etc/init.d/crond start
