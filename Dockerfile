b37ecafbbc07d3173202a53a675a3f947585b08c
core@core1 <core@core1>
2014-12-11 17:01:59 +0900

    exportディレクトリ追加等

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,12 @@
 #http://qiita.com/hnakamur/items/0b72590136cece29faee
 FROM typista/nginx-nodejs
-RUN wget https://raw.githubusercontent.com/typista/docker-mean/master/files/etc_init.d_nginx -O /root/etc_init.d_nginx && \
-	wget https://raw.githubusercontent.com/typista/docker-mean/master/files/nginx.conf -O /root/nginx.conf && \
-	wget https://raw.githubusercontent.com/typista/docker-mean/master/files/services.sh -O /root/services.sh && \
-	wget https://raw.githubusercontent.com/typista/docker-mean/master/files/monitor_node.sh -O /root/monitor_node.sh && \
+#FROM typista/nginx-nodejs:0.7
+RUN wget https://raw.githubusercontent.com/typista/docker-mean/master/files/entrypoint.sh -O /etc/entrypoint.sh && \
 	wget https://raw.githubusercontent.com/typista/docker-mean/master/files/mongodb.repo -O /etc/yum.repos.d/mongodb.repo && \
 	echo "/root/monitor_node.sh" >> /root/start.sh && \
 	yum update -y && \
 	yum -y install mongodb-org && \
-	cp /root/etc_init.d_nginx /etc/init.d/nginx && \
-	cp /root/services.sh /etc/services.sh && \
-	cp /root/nginx.conf /usr/local/nginx/conf/nginx.conf && \
-	chmod +x /etc/init.d/nginx && \
-	chmod +x /etc/services.sh && \
-	chmod +x /root/monitor_node.sh
-EXPOSE 80
-ENTRYPOINT /etc/services.sh
+	chmod +x /etc/entrypoint.sh && \
+#EXPOSE 80
+#ENTRYPOINT /etc/entrypoint.sh
 
--- files/execme1st.sh
+++ files/execme1st.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# mount:/var/www
-LOCALTIME=/etc/localtime
-if [ ! -L $LOCALTIME ]; then
-  rm $LOCALTIME
-  ln -s /usr/share/zoneinfo/Asia/Tokyo $LOCALTIME
-fi
 HOSTNAME=`hostname`
+FQDN=`echo $HOSTNAME | sed -r "s/_/\./g"`
 ROOT=/var/www/$HOSTNAME
 HTML=$ROOT/html
-REPO=mean
-BASE=https://raw.githubusercontent.com/typista/docker-$REPO/master/files/html
+BASE=$URL_GIT/html
 if [ ! -e $HTML ]; then
 	mkdir -p $HTML
 	FRONT=$HTML/front
@@ -35,24 +29,72 @@ if [ ! -e $HTML ]; then
 fi
 chown -R nginx: $ROOT
 
+NGINX_BIN=/etc/init.d/nginx
+if [ ! -f $NGINX_BIN ]; then
+	wget $URL_GIT/etc_init.d_nginx -O $NGINX_BIN 
+	chmod +x $NGINX_BIN
+fi
+
 # mount:/var/log/nginx
 LOG=/var/log/nginx/$HOSTNAME
+NGINX=/usr/local/nginx
 if [ ! -e $LOG ]; then
 	mkdir -p $LOG
 fi
-NGINX_CONF=/usr/local/nginx/conf/nginx.conf
+
+NGINX_CONF=$NGINX/conf/nginx.conf
+NGINX_CONF_TEMP=/root/export/nginx.conf
+if [ ! -f $NGINX_CONF_TEMP ];then
+	wget $URL_GIT/nginx.conf -O $NGINX_CONF_TEMP
+	cp $NGINX_CONF_TEMP $NGINX_CONF
+fi
+PROCNUM=`grep processor /proc/cpuinfo | wc -l`
 ISDEFAULT=`grep $HOSTNAME $NGINX_CONF | wc -l`
 if [ $ISDEFAULT -eq 0 ]; then
+	sed -ri "s/__PROCNUM__/$PROCNUM/g" $NGINX_CONF
 	sed -ri "s/__HOSTNAME__/$HOSTNAME/g" $NGINX_CONF
+	sed -ri "s/__FQDN__/$FQDN/g" $NGINX_CONF
 fi
-chown -R nginx: $LOG
+
+CRON_SHELL=/root/export/start.sh
+if [ ! -f $CRON_SHELL ]; then
+	wget $URL_GIT/start.sh -O $CRON_SHELL
+fi
+if [ ! -x $CRON_SHELL ]; then
+	chmod +x $CRON_SHELL
+fi
+MONITOR_NGINX=/root/export/monitor_nginx.sh
+if [ ! -f $MONITOR_NGINX ]; then
+	wget $URL_GIT/monitor_nginx.sh -O $MONITOR_NGINX
+	echo $MONITOR_NGINX >> $CRON_SHELL
+fi
+if [ ! -x $MONITOR_NGINX ]; then
+	chmod +x $MONITOR_NGINX
+fi
+MONITOR_NODE=/root/export/monitor_node.sh
+if [ ! -f $MONITOR_NODE ]; then
+	wget $URL_GIT/monitor_node.sh -O $MONITOR_NODE
+	echo $MONITOR_NODE >> $CRON_SHELL
+fi
+if [ ! -x $MONITOR_NODE ]; then
+	chmod +x $MONITOR_NODE
+fi
+CRON_TAB=/root/export/crontab.txt
+if [ ! -f $CRON_TAB ];then
+	wget $URL_GIT/crontab.txt -O $CRON_TAB
+fi
 
 MONGODB_CONF=/etc/mongod.conf
 sed -ri "s/# nojournal=true/nojournal=true/g" $MONGODB_CONF
 
-mkdir -p /data/db
 /usr/bin/mongod --smallfiles &
 crontab /root/crontab.txt
 /etc/init.d/nginx start
-/etc/init.d/crond start
-/usr/bin/tail -f /dev/null
+
+if [ -f $HTML/app.js ]; then
+	APP=app.js
+fi
+/usr/bin/forever start $APP
+
+chown -R nginx: $LOG
+crontab $CRON_TAB
